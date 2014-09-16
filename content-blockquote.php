ab0e54f8f91daaa92a6b197b73f65673fc67dc01
textiviajoshua <joshua@textivia.com>
2014-09-16 15:43:25 -0400

    commit

--- content-blockquote.php
+++ content-blockquote.php
@@ -1,7 +1,8 @@
+joshua
 	<section class="blockquote">
 		<div class="row">
 			<div class="content medium-12 large-12 columns" role="main">
 				<?php the_field('blockquote','options'); ?>
 			</div><!-- #content -->
 		</div>
-	</section>marginal
+	</section>
