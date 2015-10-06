c8df5c62466452b94e590b362957a24a5bfbaef5
textiviajoshua <joshua@textivia.com>
2015-10-06 13:06:00 -0400

    Added Blockquote tag to block quote

--- content-blockquote.php
+++ content-blockquote.php
@@ -5,3 +5,10 @@
 			</div><!-- #content -->
 		</div>
 	</section>
+	<section class="blockquote">
+		<div class="row">
+			<div class="content medium-12 large-12 columns" role="main">
+				<blockquote><?php the_field('blockquote','options'); ?></blockquote>
+			</div><!-- #content -->
+		</div>
+	</section>	
